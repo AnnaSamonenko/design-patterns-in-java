38263a0dfa484b81a628d060778fce8b05d73871
AnnaSamonenko <annsamonenko97@gmail.com>
2018-11-26 10:43:41 +0200

    Chain of responsability task added

--- src/main/java/com/gmail/samonenko/structural/chain_of_responsability/ChainOfResponsability.java
+++ src/main/java/com/gmail/samonenko/structural/chain_of_responsability/ChainOfResponsability.java
@@ -1,5 +1,44 @@
 package com.gmail.samonenko.structural.chain_of_responsability;
 
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Creature {
+    public abstract int getAttack();
+
+    public abstract int getDefense();
+}
+
+class Goblin extends Creature {
+    public Goblin(Game game) {
+        // todo
+    }
+
+    @Override
+    public int getAttack() {
+        // todo
+    }
+
+    @Override
+    public int getDefense() {
+        // todo
+    }
+}
+
+class GoblinKing extends Goblin {
+    public GoblinKing(Game game) {
+        // todo
+    }
+}
+
+enum Statistic {
+    ATTACK, DEFENSE
+}
+
+class Game {
+    public List<Creature> creatures = new ArrayList<>();
+}
+
 /**
  * Motivation:
  */
