6c7ff833f343d8741ae982cccb7e79e2a2830719
asamonenko <asamonenko@talend.com>
2019-07-09 16:45:38 +0300

    Bridge exercise

--- src/main/java/com/gmail/samonenko/structural/bridge/Bridge.java
+++ src/main/java/com/gmail/samonenko/structural/bridge/Bridge.java
@@ -1,7 +1,67 @@
 package com.gmail.samonenko.structural.bridge;
 
+
+interface Renderer {
+
+    String whatToRenderAs();
+
+}
+
+class RasterRenderer implements Renderer {
+
+    @Override
+    public String whatToRenderAs() {
+        return null;
+    }
+}
+
+class VectorRenderer implements Renderer {
+
+    @Override
+    public String whatToRenderAs() {
+        return null;
+    }
+}
+
+abstract class Shape {
+
+    protected Renderer renderer;
+
+    public Shape(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public abstract String getName();
+}
+
+class Triangle extends Shape {
+
+    public Triangle(Renderer renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public String getName() {
+        return renderer.whatToRenderAs();
+    }
+}
+
+class Square extends Shape {
+
+    public Square(Renderer renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public String getName() {
+        return renderer.whatToRenderAs();
+    }
+}
+
+
 /**
- * Motivation: TODO
+ * Motivation: Used to decouple an abstraction from it's implementation.
+ * In order to escape of Cartesian product duplication.
  */
 
 public class Bridge {
@@ -11,3 +71,5 @@ public class Bridge {
     }
 
 }
+
+
